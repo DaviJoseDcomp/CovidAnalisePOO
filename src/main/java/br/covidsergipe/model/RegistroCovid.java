2f37a8bdab35b3777dc2b4a7c1a6df8fa1651c54
DaviJoseDcomp <davi.docarmo@dcomp.ufs.br>
2025-08-15 20:25:43 -0300

    criado classe que le os dados e guarda em uma lista

--- src/main/java/br/covidsergipe/model/RegistroCovid.java
+++ src/main/java/br/covidsergipe/model/RegistroCovid.java
@@ -2,14 +2,14 @@ package br.covidsergipe.model;
 
 public class RegistroCovid {
 
-    private String data;
-    private String municipio;
-    private int casosNovos;
-    private int obitosNovos;
-    private int vacinadosNovos;
-    private int casosAcumulados;
-    private int obitosAcumulados;
-    private int vacinadosAcumulados;
+    private final String data;
+    private final String municipio;
+    private final int casosNovos;
+    private final int obitosNovos;
+    private final int vacinadosNovos;
+    private final int casosAcumulados;
+    private final int obitosAcumulados;
+    private final int vacinadosAcumulados;
 
     public RegistroCovid(String data, String municipio, int casosNovos, int obitosNovos, int vacinadosNovos, int casosAcumulados, int obitosAcumulados, int vacinadosAcumulados){
         
@@ -24,10 +24,36 @@ public class RegistroCovid {
         
 }
 
+    public String getData() {
+        return data;
+    }
 
+    public String getMunicipio() {
+        return municipio;
+    }
 
+    public int getCasosNovos() {
+        return casosNovos;
+    }
 
+    public int getObitosNovos() {
+        return obitosNovos;
+    }
 
+    public int getVacinadosNovos() {
+        return vacinadosNovos;
+    }
 
+    public int getCasosAcumulados() {
+        return casosAcumulados;
+    }
+
+    public int getObitosAcumulados() {
+        return obitosAcumulados;
+    }
+
+    public int getVacinadosAcumulados() {
+        return vacinadosAcumulados;
+    }
 
 }
---
+++ src/main/java/br/covidsergipe/service/LeitorRegistro.java
@@ -0,0 +1,34 @@
+package br.covidsergipe.service;
+
+import br.covidsergipe.model.RegistroCovid;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LeitorRegistro {
+    
+    public static List<RegistroCovid> lerArquivo(String caminho) {
+        List<RegistroCovid> registros = new ArrayList<>();
+
+        try(Scanner scanner = new Scanner(new File(caminho))) {
+            if(scanner.hasNextLine()){
+                scanner.nextLine();
+            }
+
+            while(scanner.hasNextLine()){
+                String linha = scanner.nextLine();
+                String[] campos = linha.split(",");
+                RegistroCovid registro = new RegistroCovid(campos[0], campos[1], Integer.parseInt(campos[2]), Integer.parseInt(campos[3]), Integer.parseInt(campos[4]), Integer.parseInt(campos[5]), Integer.parseInt(campos[6]), Integer.parseInt(campos[7]));
+                registros.add(registro);
+
+            }
+
+        } catch(Exception e){
+            System.out.println("Erro ao ler arquivo: " + e.getMessage());
+        }
+
+        
+        return registros;
+    }
+}
